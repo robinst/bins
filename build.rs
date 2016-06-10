f868142aa93d1fe7d9a7a138f38449c9b13a0351
Kyle Clemens <jkc.clemens@gmail.com>
2016-06-09 21:53:29 -0400

    chore(build): check for correct rustc version before compile

--- build.rs
+++ build.rs
@@ -1,13 +1,19 @@
 extern crate git2;
+extern crate rustc_version;
 
 use git2::{Repository, DescribeOptions, DescribeFormatOptions};
-
 use std::env;
 use std::path::Path;
 use std::fs::File;
-use std::io::Write;
+use std::io::{self, Write};
+use std::process::exit;
+use rustc_version::version_matches;
 
 fn main() {
+  if !version_matches(">= 1.8.0") {
+    writeln!(&mut io::stderr(), "bins requires at least Rust 1.8.0").unwrap();
+    exit(1);
+  }
   let profile = env::var("PROFILE").unwrap();
   let version = if profile == "release" {
     String::from("")
@@ -15,7 +21,10 @@ fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let repo = match Repository::open(&manifest_dir) {
       Ok(r) => r,
-      Err(e) => panic!("Could not read git repository in {}: {}", manifest_dir, e)
+      Err(e) => {
+        writeln!(&mut io::stderr(), "Could not read git repository in {}: {}", manifest_dir, e).unwrap();
+        exit(1);
+      }
     };
     let version = repo
     .describe(
